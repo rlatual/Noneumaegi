cadf18c029f072f45728ab583cacbbae24830a51
kmm <rlatual@gmail.com>
2019-08-16 15:54:10 +0900

    init

---
+++ app/src/main/java/com/nonu/noneumaegi/MainActivity.java
@@ -0,0 +1,40 @@
+package com.nonu.noneumaegi;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        drawerLayout = findViewById(R.id.dl_main_drawer_root);
+        navigationView = findViewById(R.id.nv_main_navigation_root);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_white_24dp);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(navigationView);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/nonu/noneumaegi/NoneuRegisterActivity.java
@@ -0,0 +1,13 @@
+package com.nonu.noneumaegi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoneuRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noneu_register);
+    }
+}
